2b4cc6c1d57ecce8b35701d89501101e87b7e3e1
ErdelyiAndras <andraserdelyi17@gmail.com>
2026-02-25 18:39:35 +0100

    updated ocl utils

--- src/utils/ocl-sum-utils.h
+++ src/utils/ocl-sum-utils.h
@@ -39,20 +39,51 @@ T sum(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, const
     std::copy(array, array + size, padded_array.begin());
 
     cl::Buffer array_buffer(context, CL_MEM_READ_WRITE, extended_size * sizeof(T));
-    queue.enqueueWriteBuffer(array_buffer, CL_TRUE, 0, extended_size * sizeof(T), padded_array.data());
-    queue.finish();
+    queue.enqueueWriteBuffer(array_buffer, CL_FALSE, 0, extended_size * sizeof(T), padded_array.data());
 
     kernel.setArg(0, array_buffer);
 
     for (int offset = extended_size / 2; offset > 0; offset >>= 1) {
         kernel.setArg(1, offset);
-
         queue.enqueueNDRangeKernel(kernel, cl::NullRange, offset, cl::NullRange);
-        queue.finish();
     }
 
     T result;
     queue.enqueueReadBuffer(array_buffer, CL_TRUE, 0, sizeof(T), &result);
+
+    return result;
+}
+
+template <typename T>
+T sum(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, cl::Buffer& device_data, int size) {
+    if (size == 0) {
+        return static_cast<T>(0);
+    }
+
+    cl::Kernel kernel = init_sum_kernel<T>(program);
+
+    int extended_size = 1;
+    while (extended_size < size) {
+        extended_size *= 2;
+    }
+
+    cl::Buffer scratch(context, CL_MEM_READ_WRITE, extended_size * sizeof(T));
+    queue.enqueueCopyBuffer(device_data, scratch, 0, 0, size * sizeof(T));
+
+    if (extended_size > size) {
+        T zero = static_cast<T>(0);
+        queue.enqueueFillBuffer(scratch, zero, size * sizeof(T), (extended_size - size) * sizeof(T));
+    }
+
+    kernel.setArg(0, scratch);
+
+    for (int offset = extended_size / 2; offset > 0; offset >>= 1) {
+        kernel.setArg(1, offset);
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, offset, cl::NullRange);
+    }
+
+    T result;
+    queue.enqueueReadBuffer(scratch, CL_TRUE, 0, sizeof(T), &result);
 
     return result;
 }
